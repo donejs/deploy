d6651c4e1d4f9a40fe95fd6b134c0321ac07b1e5
Mark Stahl <mjstahl@gmail.com>
2015-07-23 09:23:29 -0400

    Finished command line validation tests

--- test/test.js
+++ test/test.js
@@ -1,16 +1,27 @@
 var assert = require("assert");
 
 describe("command line argument validation", function() {
-	it("exit with an error when the argv.length is less than 3", function(done) {
-		var exec = require("child_process").exec,
-			cmd = exec("node ../bin/donejs-deploy", {
-				cwd: __dirname
-			},
-			function(error, stdout, stderr) {
+	var exec = require("child_process").exec,
+		callback = function(done, code) {
+			return function(error, stdout, stderr) {
 				if (error) {
-					assert(error.code, 1, "correctly exited with a code of 1");
+					assert(error.code, code, "correctly exited with a code of 1");
 				}
 				done();
-			});
-	})
+			}
+		};
+
+	it("should exit with an error when the argv.length is less than 3", function(done) {
+		var cmd = exec("node ../bin/donejs-deploy", {
+				cwd: __dirname
+			},
+			callback(done, 1));
+	});
+
+	it("should exit when the destination directory doesn't exist", function(done) {
+		var cmd = exec("node ../bin/donejs-deploy dist/bundles", {
+				cwd: __dirname
+			},
+			callback(done, 1));
+	});
 });
