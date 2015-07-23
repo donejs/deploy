a8a23d75e597ba22ea87c16168a29f8f2540bb88
Mark Stahl <mjstahl@gmail.com>
2015-07-23 09:17:10 -0400

    First test for missing arguments

--- test/test.js
+++ test/test.js
@@ -1,7 +1,16 @@
 var assert = require("assert");
 
-describe("we have a testing framework", function() {
-	it("should be okay", function() {
-		assert(true);
+describe("command line argument validation", function() {
+	it("exit with an error when the argv.length is less than 3", function(done) {
+		var exec = require("child_process").exec,
+			cmd = exec("node ../bin/donejs-deploy", {
+				cwd: __dirname
+			},
+			function(error, stdout, stderr) {
+				if (error) {
+					assert(error.code, 1, "correctly exited with a code of 1");
+				}
+				done();
+			});
 	})
 });
