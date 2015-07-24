c7ca1887bf06a39c559e1b1c82f17b3f57d0b99a
Mark Stahl <mjstahl@gmail.com>
2015-07-24 08:25:24 -0400

    Adjusted lines to mostly fit without 80 column width

--- test/package.js
+++ test/package.js
@@ -1,6 +1,7 @@
 var assert = require("assert");
 
-describe("donejs-package: command line argument validation", function() {
+describe("donejs-package: command line argument validation",
+function() {
 	var exec = require("child_process").exec,
 		callback = function(done, code) {
 			return function(error, stdout, stderr) {
@@ -11,28 +12,32 @@ describe("donejs-package: command line argument validation", function() {
 			}
 		};
 
-	it("should exit with an error when the argv.length is less than 3", function(done) {
+	it("should exit with an error when the argv.length is less than 3",
+	function(done) {
 		var cmd = exec("node ../bin/donejs-package", {
 				cwd: __dirname
 			},
 			callback(done, 1));
 	});
 
-	it("should exit when the destination directory doesn't exist", function(done) {
+	it("should exit when the destination directory doesn't exist",
+	function(done) {
 		var cmd = exec("node ../bin/donejs-deploy dist/bundles", {
 				cwd: __dirname
 			},
 			callback(done, 1));
 	});
 
-	it("should be able to use -d to specify a destination directory", function(done) {
+	it("should be able to use -d to specify a destination directory",
+	function(done) {
 		var cmd = exec("node ../bin/donejs-deploy -d dist/bundles", {
 				cwd: __dirname
 			},
 			callback(done, 1));
 	});
 
-	it("should be able to use --dir to specify a destination directory", function(done) {
+	it("should be able to use --dir to specify a destination directory",
+	function(done) {
 		var cmd = exec("node ../bin/donejs-deploy --dir dist/bundles", {
 				cwd: __dirname
 			},
