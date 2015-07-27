a490100576e6294de13ab5ccc7985b644420a1b1
Mark Stahl <mjstahl@gmail.com>
2015-07-27 14:37:42 -0400

    Changed the layout of test package.json to make testing multiple configurations easier
    Removed uneeded comment

---
+++ test/packages.js
@@ -0,0 +1,24 @@
+module.exports = {
+	// where glob is a string
+	"string": {
+		"donejs": {
+			"deploy": {
+				"bundles-path": "dist/bundles",
+				"glob": "assets/**/*.png",
+				"service": {
+
+				}
+			}
+		}
+	},
+
+	// where glob is an object
+	"object": {
+
+	}
+
+	// where glob is an array of strings and objects
+	"array": {
+
+	}
+}
