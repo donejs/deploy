4de34e565e5cc5abaa58e639d1fab0df368caa41
Mark Stahl <mjstahl@gmail.com>
2015-07-27 17:53:24 -0400

    Updated what the package.json will look like fully configured

--- test/packages.js
+++ test/packages.js
@@ -5,8 +5,13 @@ module.exports = {
 			"deploy": {
 				"bundles-path": "dist/bundles",
 				"glob": "assets/**/*.png",
-				"service": {
-
+				"services": {
+					"production": {
+						"type": "s3",
+						"bucket": "donejs.deploy"
+						"config-path": "path/to/config/file",
+						"default": true
+					}
 				}
 			}
 		}
