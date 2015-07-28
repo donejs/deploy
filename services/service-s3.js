4919885d0c96aebfb32c82c206c683ab31491117
Mark Stahl <mjstahl@gmail.com>
2015-07-28 11:26:52 -0400

    Finishing off fully relying upon package.json

--- services/service-s3.js
+++ services/service-s3.js
@@ -4,46 +4,50 @@ var fs = require("fs"),
 	AWS = require("aws-sdk"),
 	S3 = new AWS.S3({apiVersion: '2006-03-01'});
 
-module.exports = function(options, onError) {
-	var bucketExists = function(bucketName) {
-		console.log("Checking for the existence of " + bucketName);
+module.exports = {
+	properties: ["bucket", "config-path"],
 
-		var deferred = q.defer();
-		S3.headBucket({
-			Bucket: bucketName
-		}, function(err, resp) {
-			if (err) return deferred.reject(err);
-			deferred.resolve(true);
-		});
-		return deferred.promise;
-	};
+	deploy: function(options, Error) {
+		var bucketExists = function(bucketName) {
+			console.log("Checking for the existence of " + bucketName);
 
-	var createBucket = function(bucketName) {
-		console.log("Bucket not found. Creating bucket " + bucketName);
+			var deferred = q.defer();
+			S3.headBucket({
+				Bucket: bucketName
+			}, function(err, resp) {
+				if (err) return deferred.reject(err);
+				deferred.resolve(true);
+			});
+			return deferred.promise;
+		};
 
-		var deferred = q.defer();
-		S3.createBucket({
-			Bucket: bucketName
-		}, function(err, resp) {
-			if (err) return deferred.reject(err);
-			deferred.resolve(bucketName);
-		});
-		return deferred.promise;
-	};
+		var createBucket = function(bucketName) {
+			console.log("Bucket not found. Creating bucket " + bucketName);
 
-	var uploadFile = function(path, bucket) {
-		var fileBuffer = fs.readFileSync(path);
-		var contentType = mimeType.lookup(path);
+			var deferred = q.defer();
+			S3.createBucket({
+				Bucket: bucketName
+			}, function(err, resp) {
+				if (err) return deferred.reject(err);
+				deferred.resolve(bucketName);
+			});
+			return deferred.promise;
+		};
 
-		S3.putObject({
-			ACL: "public-read",
-			Bucket: bucket,
-			Key: path,
-			Body: fileBuffer,
-			ContentType: contentType
-		}, function(error, data) {
-			console.log("Uploaded: " + path);
-			console.log(arguments);
-		});
-	};
-};
+		var uploadFile = function(path, bucket) {
+			var fileBuffer = fs.readFileSync(path);
+			var contentType = mimeType.lookup(path);
+
+			S3.putObject({
+				ACL: "public-read",
+				Bucket: bucket,
+				Key: path,
+				Body: fileBuffer,
+				ContentType: contentType
+			}, function(error, data) {
+				console.log("Uploaded: " + path);
+				console.log(arguments);
+			});
+		};
+	}
+}
