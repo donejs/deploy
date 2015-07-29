7c8924b347941339c1dce04860ec623b07f7e738
Mark Stahl <mjstahl@gmail.com>
2015-07-29 17:36:49 -0400

    Refactored to make the functions easier to test

--- services.js
+++ services.js
@@ -2,30 +2,19 @@ var path = require("path"),
 	_ = require("lodash"),
 	fsx = require("fs-extra"),
 	glob = require("glob"),
-	pathparse = require("path-parse");
+	pathparse = require("path-parse"),
+	self = this;
 
 module.exports = {
-	list: function() {
-		var pattern = __dirname + "/services/**/service-*.js";
-		return glob.sync(pattern).map(function(servicePath) {
-			return {
-				type: pathparse(servicePath).name.split("-")[1],
-				service: require(servicePath)
-			}
-		});
+	_selected: function(specified, configured) {
+	 	return (specified && configured[specified])
+			? configured[specified]
+			: _.first(_.filter(_.values(configured), { "default": true }));
 	},
-	selected: function(package, specified, configured, available, error) {
-		var selected = (specified && configured[specified])
-				? configured[specified]
-				: _.first(_.filter(_.values(configured), { "default": true }));
-		if (!selected) { selected = _.first(_.values(configured)); }
-
-		var service =
-			_.first(_.pluck(_.where(available, {"type": selected.type}), "service"));
-		if (!service) {
-			error("'"+  selected.type +"' is not supported. ")
-		}
-
+	_service: function(available, selected) {
+		return _.first(_.pluck(_.where(available, {"type": selected.type}), "service"));
+	},
+	_properties: function(service, selected, error) {
 		_.map(service.properties, function(prop) {
 			if (!_.get(selected, prop.name)) {
 				var _default = _.get(prop.default);
@@ -40,6 +29,29 @@ module.exports = {
 				}
 			}
 		});
-		return { service: service, config: selected };
+		return selected;
+	},
+	list: function() {
+		var pattern = __dirname + "/services/**/service-*.js";
+		return glob.sync(pattern).map(function(servicePath) {
+			return {
+				type: pathparse(servicePath).name.split("-")[1],
+				service: require(servicePath)
+			}
+		});
+	},
+	selected: function(package, specified, configured, available, error) {
+		var selected = this._selected(specified, configured);
+		if (!selected) {
+			selected = _.first(_.values(configured));
+		}
+		var service = this._service(available, selected);
+		if (!service) {
+			error("'"+  selected.type +"' is not supported. ")
+		}
+		return {
+			service: service, config:
+			this._properties(service, selected)
+		};
 	}
 }
